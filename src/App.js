f0f94a9027d10aba32f3d0ff0c439f269133655f
Violet Replicon <vreplicon@gmail.com>
2019-10-16 09:26:22 -0700

    Init commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+    <main className='App'>
+      {/* content goes here */}
+    </main>
+    );
+  }
+}
+export default App;
