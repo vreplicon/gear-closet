58ef8208818dcb7b9563c12cd96db2630ce7d080
Violet Replicon <vreplicon@gmail.com>
2019-12-02 10:52:24 -0800

    Forgot to change endpoint

--- src/config.js
+++ src/config.js
@@ -1,6 +1,5 @@
 export default {
-  // API_ENDPOINT: "https://murmuring-plains-08245.herokuapp.com",
-  API_ENDPOINT: "http://localhost:8000",
+  API_ENDPOINT: "https://murmuring-plains-08245.herokuapp.com",
   CLIENT:
     "238713141249-1bq3e0r2fnedi4kg14l2mdt6jpm35937.apps.googleusercontent.com"
 };
